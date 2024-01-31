55975d876c79a4223e780cb63e95783b4fc28ea2
LucasBonii <lucasbonii2005@gmail.com>
2024-01-31 11:25:52 -0300

    mudanca bd postgresql

--- pinterest/__init__.py
+++ pinterest/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunity.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "wEl1pULTBXwU17GUgkX65aac7c28558b"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
